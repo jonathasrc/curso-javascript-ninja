1759577358564fec710ab8e961e70b924516ea90
Jonathas Rodrigues <jrc1@ifal.edu.br>
2019-04-02 00:52:23 -0300

    refactoring branch challege-01

---
+++ challenge-01/README.js
@@ -0,0 +1,53 @@
+/*#Desafio Semana #1*/
+
+/*```js*/
+
+// Declarar uma variável chamada 'myvar', sem valor.
+
+var myvar;
+
+// Após declarada, atribua o valor 10 à variável 'myvar'.
+myvar = 10;
+
+// Declare uma nova variável chamada 'sum', e adicione uma instrução somando os valores 15 e 8.
+var sum = 15 + 8;
+
+// Atribua à variável 'sum' todo o valor dela, somando 1, usando o operador de soma abreviado.
+sum++;
+
+// Atribua à variável 'sum' todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
+sum *= 3;
+
+// Qual é o valor da variável 'sum' até aqui?
+72
+
+// Declare uma variável chamada 'souninja', atribuindo à ela o valor booleano que representa 'verdadeiro'.
+var souninja = true;
+
+// Declare uma variável chamada 'food' que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
+
+var food = ['arroz','feijao','ovo']
+
+// Digite a instrução que imprime o valor de 'feijao', que está na variável 'food'.
+
+food[1]
+
+// Digite o código que verifica se a variável 'sum' é igual a variável 'myvar' (testando também o tipo).
+
+myvar === sum
+
+// Digite o código que verifica se a variável 'myvar' é menor ou igual à variável 'sum'.
+
+myvar <= sum
+
+// Crie uma função chamada 'division' que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
+
+function division(arg1,arg2){
+  return arg1 / arg2;
+}
+
+// Invoque a função criada acima, passando os parâmetros 10 e 2.
+
+division(10,2);
+
+/*```*/
